cc68d1fd287cb8dc0151b00284c9cd6c2ac1e117
Alessandra <alezzix@gmail.com>
2018-10-10 01:04:30 -0300

    Update FortuneCookieBuilder.java

--- src/main/java/com/monsanto/interview/FortuneCookieGenerator/FortuneCookieBuilder.java
+++ src/main/java/com/monsanto/interview/FortuneCookieGenerator/FortuneCookieBuilder.java
@@ -7,11 +7,13 @@ import org.springframework.stereotype.Component;
 @Component
 @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class FortuneCookieBuilder {
-	    private static final String NOT_DEFINED = "N/A";
-
+	
+    private static final String NOT_DEFINED = "N/A";
     private String client = NOT_DEFINED;
     private String company = NOT_DEFINED;
     private String quote = NOT_DEFINED;
+
+//Deny was included in if, as well as removal of this "from" verification:
 
     FortuneCookieBuilder withClient(String client) {
         if (!client.equals(NOT_DEFINED))
